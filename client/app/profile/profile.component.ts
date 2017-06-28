54979b4a9ad2b04832f96ebad000b513704f09be
Karl Noelle <karl.noelle@gmail.com>
2017-06-27 22:20:36 -0500

    made changes to it.

--- client/app/profile/profile.component.ts
+++ client/app/profile/profile.component.ts
@@ -6,28 +6,34 @@ const ngRoute = require('angular-route');
 import routes from './profile.routes';
 
 export class ProfileComponent {
-  //message: string;
+  $location;
   isLoggedIn: Function;
   getCurrentUser: Function;
-  /*@ngInject*/
-  constructor(Auth) {
-    //this.message = 'Hello what up where does this message appear. im coming from profile.component.ts';
 
+  constructor($location, Auth) {
     'ngInject';
+    this.$location = $location;
     this.isLoggedIn = Auth.isLoggedInSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
   }
 
-  /*isActive(route) {
+  isActive(route) {
     return route === this.$location.path();
-  }*/
+  }
 }
+
+/*export default angular.module('directives.navbar', [])
+  .component('navbar', {
+    template: require('./navbar.html'),
+    controller: NavbarComponent
+  })
+  .name;*/
 
 export default angular.module('runpurposeApp.profile', [ngRoute])
   .config(routes)
   .component('profile', {
     template: require('./profile.html'),
     controller: ProfileComponent,
-    controllerAs: 'profileCtrl'
+    //controllerAs: 'profileCtrl'
   })
   .name;
