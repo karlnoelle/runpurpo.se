ef11fd9ff3fc2eee0fdc2f683a46c7ab2c9228c6
Karl Noelle <karl.noelle@gmail.com>
2018-08-01 08:05:36 -0500

    minnyScript 7/31 updates

---
+++ client/js/create-event.js
@@ -0,0 +1,39 @@
+$(function () {
+    let $form = $('.create-event-form');
+
+    function submitForm () {
+        let name = $form.find('.event-name input').val();
+        let date = $form.find('.event-date input').val();
+        let time = $form.find('.event-time input').val();
+        let location = $form.find('.event-location input').val();
+        let address = $form.find('.event-address input').val();
+        let description = $form.find('.event-description input').val();
+        // Add validation, default values.
+
+        let event = {
+            'name': name,
+            'date': date,
+            'time': time,
+            'location': location,
+            'address': address,
+            'description': description,
+        };
+        $.ajax({
+            contentType: 'application/json',
+            data: JSON.stringify(event),
+            error: function(data, sStatus) {
+                alert('you failed! ' + sStatus);
+            },
+            method: 'POST',
+            success: function(data, sStatus) {
+                // TODO: redirect.
+                alert('success!');
+            },
+            url: '/create-event',
+        });
+    }
+    $('button.submit-form').click(function(e) {
+        e.stopPropagation();
+        submitForm();
+    });
+});
--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const passport = require('passport');
 const process = require('process');
 const Strategy = require('passport-local').Strategy
 const app = express();
+var bodyParser = require('body-parser');
 
 var users = [
     {id: 1, username: 'rick', password: 'petty', displayName: 'rp', emails: [ { value: 'rp@aol.net'} ] }
@@ -17,16 +19,39 @@ passport.use(new Strategy(
     }
 ));
 
+app.use(bodyParser.json()); // for parsing application/json
+
 app.use(express.static("client"));
 app.post('/login', passport.authenticate('local'));
 app.get('/profile', function (request, response) {
     // TODO: validate user
     let valid = true;
     if (valid) {
-        response.sendFile(process.cwd() + '/client/profile.html');
+    response.sendFile(process.cwd() + '/client/profile.html');
     } else {
         response.redirect('/');
     }
 });
+app.post('/create-event', function (request, response) {
+    console.log('received event: ' + JSON.stringify(request.body));
+    // TODO: validation
+    // TODO: save --> id
+    response.sendStatus(201);
+});
+app.get('/event', function (request, response) {
 
+    // TODO: load event from DB
+});
+
+// Open connection to MongoDB
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+
+//listen for a connecion in the browser
 app.listen(3000, () => console.log('runpurpose is listening on port 3000'));
