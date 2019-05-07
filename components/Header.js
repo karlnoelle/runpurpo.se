62d9423d93bb37689aae843d57966e7f8e3318ca
Karl Noelle <karl.noelle@gmail.com>
2019-05-07 17:26:42 -0500

    committing files

--- components/Header.js
+++ components/Header.js
@@ -1,39 +1,40 @@
 import Link from 'next/link'
+import logo from '../assets/img/runpurpose-logo.svg'
 
 const linkStyle = {
   marginRight: 15
 }
 
 const Header = () => (
-  <Fragment>
+  <div>
     <header>
-    <div class="header-wrapper">
+      <div class="header-wrapper">
         <div class="header-left">
-            <a routerLink="/">
-                <img src="/src/assets/img/runpurpose-logo.svg" />
-            </a>
+          <a routerLink="/">
+            <logo />
+          </a>
         </div>
         <div class="header-right">
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/events">Events</a>
-                </li>
-                <li>
-                    <a href="/">Login/Signup</a>
-                </li>
-                <li>
-                    <a href="/admin">Admin</a>
-                </li>
-            </ul>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/events">Events</a>
+            </li>
+            <li>
+              <a href="/">Login/Signup</a>
+            </li>
+            <li>
+              <a href="/admin">Admin</a>
+            </li>
+          </ul>
         </div>
-    </div>
-</header>
+      </div>
+    </header>
 
     <Link href="/">
       <a style={linkStyle}>Home</a>
@@ -41,7 +42,7 @@ const Header = () => (
     <Link href="/about">
       <a style={linkStyle}>About</a>
     </Link>
-  </Fragment>
+  </div>
 )
 
 export default Header
